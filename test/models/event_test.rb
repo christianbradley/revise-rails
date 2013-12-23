27e18eff729b2fb9c1220dec6b0c22aa596440e6
Christian Bradley <christianbradley@gmail.com>
2013-12-22 23:52:51 -0500

    Event serializes payload as JSON

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_serializes_json
+    hash = { "some" => "json" }
+    event = Event.new type_name: "UserRegistered", occurred_at: Time.zone.now, payload: hash
+    event.save!
+
+    event.reload
+
+    assert_equal event.payload, hash
+  end
 end
